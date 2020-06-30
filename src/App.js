846db39cf3c7d9ebc0425ca5263ab6dd86034fab
gladysleng <gladys_yinyuen@hotmail.com>
2020-06-29 23:47:07 -0400

    fixed version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import './App.css';
-
-
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
+  )
+}
+export default App;
