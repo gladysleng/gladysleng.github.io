c008014f0fd915e8315382bb4eeae29344e3cd55
gladysleng <gladys_yinyuen@hotmail.com>
2020-07-05 01:18:09 -0400

    fixing homePage

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactTypingEffect from 'react-typing-effect';
-import profilepic from '../img/profile.png';
 import Social from '../components/Social';
 
 
@@ -8,7 +7,6 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv-home" >
-                <img src={profilepic} className="profilepic"></img><br/>
                 <ReactTypingEffect className="typingeffect" text={['I am Yin Yuen Leng', 'I am a software developer']} speed={300} eraseDelay={100} />
             </div>
         )
