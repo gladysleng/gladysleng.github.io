8989f0f6ada43e68036c9df370525ed7a044cf47
gladysleng <gladys_yinyuen@hotmail.com>
2020-07-08 16:52:32 -0400

    finish home page

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Social from '../components/Social';
-
+import Typical from 'react-typical';
+import YinResume from '../YinResume.pdf';
+import styled from "styled-components";
 
 class Home extends Component {
     render() {
+        const Button = styled.button`
+        background-color: black;
+        color: white;
+        font-size: 15px;
+        padding: 5px 10px;
+        border-radius: 30px;
+        margin: 70px 0px;
+        cursor: pointer;
+      `;
         return (
             <div className="condiv-home" >
-                <ReactTypingEffect className="typingeffect" text={['I am Yin Yuen Leng', 'I am a software developer']} speed={300} eraseDelay={100} />
+                <h1 style={{ fontFamily: '"Playfair Display", Georgia, serif', fontSize: '60px',marginTop:'-10px' }}>Hi!<br /> I'm Yin Yuen</h1>
+                <span className="home-introduction"> I'm {' '}
+                    <Typical
+                        loop={Infinity}
+                        wrapper="b"
+                        steps={[
+                            'a fresh grad 🎓   from Carleton University ',
+                            700,
+                            'a software developer 👩‍💻 ',
+                            700,
+                            'open to all opportunities 🚪',
+                            700,
+                        ]}
+                    />
+                </span>
+                    <a href={YinResume} target="_blank" rel="noopener noreferrer" download>  
+                        <Button>
+                            Download CV{' '}
+                            <i className="fa fa-download" />
+                        </Button>
+                    </a>
             </div>
         )
     }
