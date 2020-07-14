6480ca772dc39cd00e066b62d0f98d70b90a46c7
gladysleng <gladys_yinyuen@hotmail.com>
2020-07-14 17:12:52 -0400

    minor fix on writings

--- src/contents/About.js
+++ src/contents/About.js
@@ -6,18 +6,18 @@ class About extends Component {
                 <h1 className="subtopic" >About Me</h1>
                 <br></br>
                 <p className="about">
-                    I came to Canada to pursue my Computer Science degreee in 2015, at Carleton
-                    University in Ottawa. I started my first programming course with absolute zero
-                    knowledge in programming. At the begining of my journey in learning about the world
-                    of computer science, I find programming concepts were hard to grasp as it requires
-                    an different way of thinking compares to other subjects. However, with persistant and patient,
+                    I came to Canada to pursue my Computer Science Bachalor Degree in 2015, at Carleton
+                    University in Ottawa. At the begining,  I find programming concepts were hard to grasp as it requires
+                    different way of thinking compares to what I'm used to. I started my first programming course with absolute zero
+                    knowledge in programming. Quite frankly, I've been through a lot of debate and doubting myself if I made the right choice
+                    to pursue a degree in CS. However, with persistant and patient,
                     I became better and better in programming and developed a huge interest in this field.
-                    I have learned that a successful programmer is not about how clean the code is or being expert
-                    at different languages, but the way the programmer thinks and approaches a problem is far more critical.
+                    I have learned that a successful programmer is not just about writing solid code or being expert
+                    at different languages, but the way a programmer thinks and approaches a problem is far more critical.
                 </p>
                 <p className="about">
                     As the world of teachnology keeps revolving, I took every opportunities to attend tech related
-                    conferences and hackathons to learn from other professionals and broaden my knowledge.I'm amazed
+                    conferences and hackathons to learn from other professionals and broaden my knowledge. I'm amazed
                     at the positive impact of technology in our everyday life and how coding influence the world.
                     I look up to the innovators and hoping to be one of them who create a better future for humankind.
                     Having the thoughts of being able to contribute to the society with the knowledge I acquired
