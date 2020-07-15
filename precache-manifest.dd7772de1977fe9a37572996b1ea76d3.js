07887df43d2520bca7707d9b5c77af3dc8c8840f
gladysleng <gladys_yinyuen@hotmail.com>
2020-07-15 16:58:36 -0400

    Updates

---
+++ precache-manifest.dd7772de1977fe9a37572996b1ea76d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f251dc1dd7ade19d21cf084673816d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9921b84b50ef953d4d7",
+    "url": "/static/css/2.af50cfed.chunk.css"
+  },
+  {
+    "revision": "919828851b213d75bc67",
+    "url": "/static/css/main.cde8aea0.chunk.css"
+  },
+  {
+    "revision": "c9921b84b50ef953d4d7",
+    "url": "/static/js/2.d9a0bea8.chunk.js"
+  },
+  {
+    "revision": "c0a7484cc31358a0df86ce48ef08f02b",
+    "url": "/static/js/2.d9a0bea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919828851b213d75bc67",
+    "url": "/static/js/main.4cc9a0dd.chunk.js"
+  },
+  {
+    "revision": "c105a6bae94f7a137e85",
+    "url": "/static/js/runtime-main.362a5223.js"
+  },
+  {
+    "revision": "4352d93b1f8c55151477f44682ada05e",
+    "url": "/static/media/YinResume.4352d93b.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b3bdbf907acf17c62841d844b54c7fe1",
+    "url": "/static/media/homeImage1.b3bdbf90.png"
+  },
+  {
+    "revision": "852307f43348ce21e17e667090b352ce",
+    "url": "/static/media/homeImage4.852307f4.png"
+  },
+  {
+    "revision": "8338cdd0ffc68c191c3e70d8ee43b31d",
+    "url": "/static/media/profile.8338cdd0.png"
+  }
+]);
